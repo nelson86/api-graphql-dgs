7a40471521743db612783aa390f0c40783be3264
nelson arabel <nelson@nelsons-MacBook-Pro.local>
2022-08-24 12:44:11 -0300

    first commit

---
+++ src/main/kotlin/com/narabel/api_graphql_dgs/ApiGraphqlDgsApplication.kt
@@ -0,0 +1,11 @@
+package com.narabel.api_graphql_dgs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGraphqlDgsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGraphqlDgsApplication>(*args)
+}
---
+++ src/test/kotlin/com/narabel/api_graphql_dgs/ApiGraphqlDgsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.narabel.api_graphql_dgs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGraphqlDgsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
